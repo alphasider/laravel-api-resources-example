11b2101f3358778350977296d3c6b914e13c1641
Rustam <rustamergashev.sp@gmail.com>
2020-07-03 00:24:49 +0300

    Все CRUD операции реализованы через http зÐарпросы

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Добавление нового участника
+Route::post('participants', 'API\ParticipantController@store');
+
+// Получение списка всех участников
+Route::get('participants', 'API\ParticipantController@index');
+
+// Получение участника по ID
+Route::get('participants/{id}', 'API\ParticipantController@show');
+
+// Удаление участника по ID
+Route::delete('participants/{id}', 'API\ParticipantController@delete');
+
+// Редактирование участника по ID
+Route::put('participants/{id}', 'API\ParticipantController@update');
